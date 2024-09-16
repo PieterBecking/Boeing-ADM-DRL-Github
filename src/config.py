18a019b8defd612921a7206eda229a830dec4b2c
Pieter Becking <ph.becking@gmail.com>
2024-09-16 17:47:53 +0200

    training settings in config.py

--- src/config.py
+++ src/config.py
@@ -7,10 +7,8 @@ COLUMNS_STATE_SPACE = 1 + 2 + 3 * MAX_FLIGHTS_PER_AIRCRAFT # Number of columns i
 # Time Settings for intervals
 TIMESTEP_HOURS = 1  # Length of each timestep in hours
 
-# Hyperparameters for the training agent
+
 DUMMY_VALUE = -999  # Dummy value for padding
-LEARNING_RATE = 0.001  # Learning rate for RL agent
-EPOCHS = 100  # Number of training epochs
 
 # Reward and Penalty Values
 DELAY_PENALTY_PER_MINUTE = 5  # Penalty per minute of delay
@@ -34,3 +32,15 @@ DEBUG_MODE_REWARD = True  # Turn on/off debug mode for reward calculation
 
 # Data Generation Settings
 DEPARTURE_AFTER_END_RECOVERY = 1  # how many hours after the end of the recovery period can a generated flight depart
+
+
+# Training Settings
+LEARNING_RATE = 0.001
+GAMMA = 0.99
+BUFFER_SIZE = 50000
+BATCH_SIZE = 32
+TARGET_UPDATE_INTERVAL = 100
+EPSILON_START = 1.0
+EPSILON_MIN = 0.05
+EPSILON_DECAY_RATE = 0.0005
+MAX_TIMESTEPS = 500         # maximum number of timesteps per episode (not relevant here)
